024328a74ae0342f49cc0f9bb06ec793d2f76f08
KalebHouse <root@kapitalk.com>
2019-11-10 04:17:40 +0000

    Adding night background, fixing many layout problems, and adding basic layout support for mobile.

--- kapitalk-site/js/main.js
+++ kapitalk-site/js/main.js
@@ -19,3 +19,42 @@ function scrollTo(element, to, duration) {
     scrollTo(element, to, duration - 2);
   }, 10);
 }
+
+var pages = ['aboutMe', 'projects'];
+function loadPage(page) {
+  var bgNight = document.getElementById('backgroundNight');
+  var bgDay = document.getElementById('backgroundDay');
+
+  switch(page) {
+    case 'aboutMe':
+      document.getElementById('aboutMe-nav').classList.add('active');
+      document.getElementById('projects-nav').classList.remove('active');
+      bgDay.classList.remove('fadeOut');
+      bgDay.classList.add('fadeIn');
+      bgNight.classList.add('fadeOut');
+      bgNight.classList.remove('fadeIn');
+      document.getElementById('projects').style.opacity = '0';
+      document.getElementById('projects').style.zIndex = '0';
+      setTimeout( function() {
+        document.getElementById('aboutMe').style.opacity = '1';
+        document.getElementById('aboutMe').style.zIndex = '5';
+      }, 1000);
+      //content
+      break;
+    case 'projects':
+      document.getElementById('aboutMe-nav').classList.remove('active');
+      document.getElementById('projects-nav').classList.add('active');
+      bgNight.classList.remove('fadeOut');
+      bgNight.classList.add('fadeIn');
+      bgDay.classList.add('fadeOut');
+      bgDay.classList.remove('fadeIn');
+      document.getElementById('aboutMe').style.opacity = '0';
+      document.getElementById('aboutMe').style.zIndex = '0';
+      setTimeout( function() {
+        document.getElementById('projects').style.opacity = '1';
+        document.getElementById('projects').style.zIndex = '5';
+      }, 1000);
+      //content
+      break;
+  }
+}
